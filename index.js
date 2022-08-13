915d45064130a66dfdf7182bf04e965455b8ccd2
Aras emr <arasemr1234@gmail.com>
2022-08-13 17:49:58 +0300

    3.1.0 - layouts

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+const fs = require('fs');
+
 function m(data,values) {
+    let fmatch = data.match(/\{\{\-(.*?)\}\}/gm);
     let match = data.match(/\{\{(.*?)\}\}/gm);
+
+    if(fmatch){
+        fmatch.forEach((m) => {
+            let frm = m.replace(/\{\{|\}\}/g, '').trim();
+            if(frm.startsWith("-")){
+                frm = frm.slice(1);
+                let cu = fs.readFileSync(frm+'.eme','utf-8');
+                data = data.replaceAll(`{{-${frm}}}`,cu);
+            }
+        });
+    }
 
     if(match){
         match.forEach((m) => {
             let frm = m.replace(/\{\{|\}\}/g, '').trim();
-            if(!frm.startsWith("=")){
+            if(!frm.startsWith("=") && !frm.startsWith("-")){
                 data = data.replaceAll(`{{${frm}}}`,eval(frm));
             }
         });
     }
 
-    values.forEach((value) => {
-        data = data.replaceAll(`{{=${value.name}}}`,value.value);
-        data = data.replaceAll(`{{=${value.name}.length}}`,value.value.length);
-    });
+    if(values){
+        values.forEach((value) => {
+            data = data.replaceAll(`{{=${value.name}}}`,value.value);
+            data = data.replaceAll(`{{=${value.name}.length}}`,value.value.length);
+        });
+    }
 
     return data;
 }
@@ -26,7 +42,7 @@ function compile(data,values) {
 }
 
 async function compilefile(data,values) {
-    if(!data.split(".")[1] || !data.split(".")[1] === "eme") return console.log("Only supported .eme file extensions.");
+    if(!data.split(".")[1] || data.split(".")[1] != "eme") return console.log("Only supported .eme file extensions.");
     const fs = require('fs');
     let filedata = await fs.readFileSync(data,'utf-8');
 
